ee37f33e9b010c7206df56ce2e47caa16989d5e5
Jane Nguyen <jane.nguyen1@monash.edu>
2024-11-25 11:52:46 +1100

    Correct support for addition

--- calc.py
+++ calc.py
@@ -6,7 +6,7 @@ def compute(expression):
     if operator == '+':
         return num0 + num1
     elif operator == '-':
-        return num0 + num1
+        return num0 - num1
     elif operator == '*':
         return num0 * num1
     else:
